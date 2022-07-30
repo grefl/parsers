0476b94140433f8614e740a8c7c537f2f2dfaf93
grefl <grefl@protonmail.com>
2022-07-30 22:37:43 +0200

    v2 - a start

---
+++ markdown/v2/lexer.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python3
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+from pprint import pprint
+import unittest
+
+class TokenType(Enum):
+    Hash_Tag          = '#'
+    Asterix           = '*' 
+    UnderScore        = '_'
+    Dash              = '-'
+    LeftBracket       = '['
+    RightBracket      = ']'
+    LeftParen         = '('
+    RightParen        = ')'
+    BlockQuoteStart   = '>'
+    Shebang           = '!'
+    Tilda             = '`'
+    Ordered_List_Item = 0
+    Text              = 1
+    NewLine           = 2
+    Unknown           = 3 
+
+@dataclass
+class Token:
+    file_name: str
+    type_: TokenType
+    value: str
+
+
+
+def is_whitespace(token):
+    return token == ' ' or token == '\n' or token == '\t'
+
+def is_alpha(token):
+    return token >= 'a' and token <= 'z' or token >= 'A' and token <= 'Z'
+
+def is_numeric(token):
+    return token >= '1' and token <= '9'
+
+def is_alphanumeric(token):
+    return is_alpha(token) or is_numeric(token)
+
+class Lexer:
+    def __init__(self, string):
+        self.tokens        = []
+        self.idx           = 0
+        self.current_token = None
+        self.string = string
+        self.len = len(self.string)-1
+
+    def peek(self):
+        if self.idx + 1 >= self.len:
+            return None
+        return self.string[self.idx + 1]
+
+    def lex(self):
+
+        while self.idx <= self.len:
+            token = self.string[self.idx]
+            if token == '#':
+                self.tokens.append(Token(None, TokenType.Hash_Tag, token))
+            elif token == '*':
+                self.tokens.append(Token(None, TokenType.Asterix, token))
+            elif token == '_':
+                self.tokens.append(Token(None, TokenType.UnderScore, token))
+            elif token == '-':
+                self.tokens.append(Token(None, TokenType.Dash, token))
+            elif token == '[':
+                self.tokens.append(Token(None, TokenType.LeftBracket, token))
+            elif token == ']':
+                self.tokens.append(Token(None, TokenType.RightBracket, token))
+            elif token == '(':
+                self.tokens.append(Token(None, TokenType.LeftParen, token))
+            elif token == ')':
+                self.tokens.append(Token(None, TokenType.RightParen, token))
+            elif token == '>':
+                self.tokens.append(Token(None, TokenType.BlockQuoteStart, token))
+            elif token == '!':
+                self.tokens.append(Token(None, TokenType.Shebang, token))
+            elif token == '~':
+                self.tokens.append(Token(None, TokenType.Tilda, token))
+            elif is_whitespace(token):
+                if token == '\n':
+                    self.tokens.append(Token(None, TokenType.NewLine, token))
+            elif is_alphanumeric(token):
+                lexed_token = self.try_to_lex_text(token)
+                self.tokens.append(Token(None, TokenType.Text, lexed_token))
+                continue
+            else:
+                self.tokens.append(Token(None, TokenType.Unknown, token))
+            self.idx +=1
+
+        return self.tokens
+
+    def try_to_lex_text(self, token):
+        lexed_token = []
+
+        while self.idx <= self.len and is_alphanumeric(self.string[self.idx]):
+            token = self.string[self.idx]
+            lexed_token.append(token) 
+            self.idx +=1
+        return ''.join(lexed_token)
+
+def unwind_tokens(tokens):
+    """
+    This function unwinds the tokens given to it and instead returns the values as a string.
+
+    Example: 
+
+    ==========================================================================================
+    input  = [Token(None, TokenType.Hash_Tag, '#'), Token(None, TokenType.RightParen, ')')]
+    output = unwind_tokens(input)
+    print(output) -> '#)' 
+    ==========================================================================================
+    """
+    return ''.join([token.value for token in tokens])
+
+def unwind_tokens_with_type(tokens):
+    """
+    This function unwinds the tokens given to it and instead returns an
+    array of tuples with the first value as the TokenType and the second 
+    as the literal value.
+
+    Example: 
+
+    ==========================================================================================
+    input  = [Token(None, TokenType.Hash_Tag, '#'), Token(None, TokenType.RightParen, ')')]
+    output = unwind_tokens(input)
+    print(output) -> [(TokenType.Hash_Tag, '#'), (TokenType.RightParen, ')')] 
+    ==========================================================================================
+    """
+    return [(token.type_, token.value) for token in tokens]
+
+class Test(unittest.TestCase):
+
+    def test_1(self):
+        input_string = '#*_-[]'
+        expected = '#*_-[]'
+
+        p = Lexer(input_string)
+        tokens = p.lex()
+        actual = unwind_tokens(tokens) 
+
+        self.assertEqual(expected, actual)
+
+    def test_2(self):
+        input_string = '#*'
+        expected = [(TokenType.Hash_Tag, '#'), (TokenType.Asterix, '*')]
+
+        p = Lexer(input_string)
+        tokens = p.lex()
+        actual = unwind_tokens_with_type(tokens) 
+        
+        self.assertEqual(expected, actual)
+
+    def test_3(self):
+        input_string = '[name](url)' 
+        expected = input_string 
+
+        p = Lexer(input_string)
+        tokens = p.lex()
+        actual = unwind_tokens(tokens) 
+
+        self.assertEqual(expected, actual)
+
+def main():
+
+    file_string = Path('./alacritty.md').read_text()
+
+    p = Lexer(file_string)
+
+    tokens = p.lex()
+
+    pprint(tokens)
+
+if __name__ == "__main__":
+    unittest.main()
+    # main()
+
+        
---
+++ markdown/v2/parser.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+from pprint import pprint, pformat
+import unittest
+from lexer import Lexer, TokenType
+# lambda x: x.type_ != TokenType.RightParen or x.type_ != TokenType.NewLine
+def DEBUG(x): 
+    if x.type_ == TokenType.NewLine or x.type_ == TokenType.RightParen:
+        return False
+    return True
+
+class ParsedTokenType(Enum):
+    H1 = 0
+    H2 = 1 
+    H3 = 2 
+    H4 = 3 
+    H5 = 4 
+    H6 = 5 
+    P  = 6
+    A  = 7
+    IMG = 8
+
+    Blockquote = 9
+@dataclass
+class ParsedToken:
+    type_: ParsedTokenType
+    value: str 
+class Parser:
+
+    def __init__(self, tokens):
+        self.tokens        = tokens
+        self.len           = len(self.tokens) -1
+        self.idx =         -1 
+        self.intermediate  = []
+        self.prev_token    = None
+        self.current_token = None
+        self.next_token    = None
+
+    def peek_token(self):
+        if self.idx + 1 >= self.len:
+            return None
+        return self.tokens[self.idx +1]
+    def get_token(self):
+        if self.idx + 1 >= self.len:
+            self.current_token = None
+            self.next_token    = None
+            return None
+        self.idx +=1
+        self.prev_token = self.current_token
+        self.current_token = self.tokens[self.idx]
+        self.next_token = self.peek_token()
+        return self.current_token
+            
+    def parse(self):
+        while self.get_token() is not None: 
+            if self.current_token.type_ == TokenType.Hash_Tag:
+                print('h1')
+                if self.prev_token is None or self.prev_token.type_ == TokenType.NewLine:
+                    # This token is a header. Now time to find what type - h1 or h2, etc.
+                    print('is none')
+                    first_hash = self.current_token
+                    h1s = self.parse_while(lambda x: x.type_ == TokenType.Hash_Tag)
+                    print(h1s)
+                    # header_text = self.parse_while(lambda x: x.type_ != TokenType.NewLine)
+                    header_text = self.parse_inner_block()
+                    if len(h1s) == 1:
+                        print('here')
+                        self.intermediate.append(ParsedToken(ParsedTokenType.H1, header_text))
+            elif self.current_token.type_ == TokenType.BlockQuoteStart:
+                symbol = self.current_token
+                block_quote_text = self.parse_inner_block() 
+                self.intermediate.append(ParsedToken(ParsedTokenType.Blockquote, block_quote_text))
+            elif self.current_token.type_ == TokenType.NewLine:
+                print('new line')
+                continue
+            else:
+                inner_tokens = self.parse_inner_block()
+                P = ParsedToken(ParsedTokenType.P, inner_tokens)
+                self.intermediate.append(P)
+        return self.intermediate
+    def parse_inner_block(self):
+        inner_tokens = []
+        while self.current_token is not None and self.current_token.type_ != TokenType.NewLine:
+            print('here')
+            if self.current_token.type_ == TokenType.Shebang and self.next_token.type_ == TokenType.LeftBracket:
+                print('attempt')
+                _shebang = self.current_token
+                maybe_image = self.parse_while(DEBUG)
+                print('done')
+                if self.current_token.type_ == TokenType.RightParen:
+                    maybe_image.append(self.current_token)
+                    inner_tokens.append(ParsedToken(ParsedTokenType.IMG, maybe_image))
+                else:
+                    inner_tokens.append(maybe_image)
+            else:
+                inner_tokens.append(self.current_token)
+            self.get_token()
+        return inner_tokens
+    def parse_while(self, predicate):
+        parsed_token = []
+        while self.current_token is not None and predicate(self.current_token):
+            if self.current_token.type_ == TokenType.RightParen:
+                print('got it')
+                print(self.current_token.type_ != TokenType.RightParen)
+            print('why')
+            parsed_token.append(self.current_token)
+            self.get_token()
+        return parsed_token
+
+def main():
+    file_string = Path('./image.md').read_text()
+
+    p = Lexer(file_string)
+
+    print("BEGIN LEX")
+    tokens = p.lex()
+    pprint(tokens)
+
+    print("BEGIN PARSE")
+    p = Parser(tokens)
+    parsed_tokens = p.parse()
+    formatted = pformat(parsed_tokens, width=1)
+    Path('./debug_tokens').write_text(formatted)
+
+
+if __name__ == "__main__":
+    main()
